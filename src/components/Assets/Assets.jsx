218bf837171aa1716b50849ab5f9a1c323b50508
Ashab Abidi <ashababidi@gmail.com>
2023-12-26 11:19:18 +0530

    Added the note to Homepage

--- src/components/Assets/Assets.jsx
+++ src/components/Assets/Assets.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
-import { Backdrop, Box, Button, Modal, Fade,Dialog, TextField, DialogTitle, DialogContent, Divider, DialogActions} from "@mui/material";
+import {
+  Backdrop,
+  Box,
+  Button,
+  Modal,
+  Fade,
+  Dialog,
+  TextField,
+  DialogTitle,
+  DialogContent,
+  Divider,
+  DialogActions,
+} from "@mui/material";
 // import styled from "@emotion/styled";
 import AddIcon from "@mui/icons-material/Add";
 
-
 // This is the Home button for adding the Notes
-export const AddNoteBtn = ({handleClick}) => {
+export const AddNoteBtn = ({ handleClick }) => {
   return (
     <Box
       position="fixed"
@@ -16,7 +27,7 @@ export const AddNoteBtn = ({handleClick}) => {
       boxShadow="0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
     >
       <Button
-      onClick={handleClick}
+        onClick={handleClick}
         sx={{ color: "#0d0d0d", borderRadius: "50px", padding: "1rem 2rem" }}
       >
         <AddIcon />
@@ -25,29 +36,46 @@ export const AddNoteBtn = ({handleClick}) => {
   );
 };
 
-
 // This is the modal for Adding the Notes
-export const AddNoteDialog = ({open , close}) =>{
+export const AddNoteDialog = ({ open, close, addNote }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
-    return(
-      
-       <Dialog 
-       open={open}
-       onClose={close}
-       fullWidth
-       sx={{textAlign:'center'}}
-       >
-       <DialogTitle>Notes Form</DialogTitle>
-       <Divider>Add Note</Divider>
-       <DialogContent>
-       <Box display='flex' flexDirection='column' gap='1.5rem' padding='1% 3%'>
-       <TextField variant="outlined" label='Title' placeholder="Enter Title..."/>
-       <TextField variant="outlined" label='Description' multiline rows={4} placeholder="Enter Description..."/>
-       </Box>
-       <DialogActions>
-       <Button>Add</Button>
-       </DialogActions>
-       </DialogContent>
-       </Dialog>
-    )
-}
+  const handleAddNote = () => {
+    if (title !== "" && description !== "") {
+      addNote(title, description);
+    }
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <Dialog open={open} onClose={close} fullWidth sx={{ textAlign: "center" }}>
+      <DialogTitle>Notes Form</DialogTitle>
+      <Divider>Add Note</Divider>
+      <DialogContent>
+        <Box display="flex" flexDirection="column" gap="1.5rem" padding="1% 3%">
+          <TextField
+            variant="outlined"
+            label="Title"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
+            placeholder="Enter Title..."
+          />
+          <TextField
+            variant="outlined"
+            label="Description"
+            multiline
+            value={description}
+            onChange={(e)=>setDescription(e.target.value)}
+            rows={4}
+            placeholder="Enter Description..."
+          />
+        </Box>
+        <DialogActions>
+          <Button onClick={handleAddNote}>Add</Button>
+        </DialogActions>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,12 +9,13 @@ import {
   Typography,
   Button,
   Grid,
+  CardHeader,
 } from "@mui/material";
 import { AddNoteBtn, AddNoteDialog } from "../Assets/Assets";
 
 const Home = () => {
   const [openModal, setOpenModal] = useState(false);
-
+  const [data , setData] = useState([]);
 
 
   const handleOpen = () =>{
@@ -24,14 +25,36 @@ const Home = () => {
     setOpenModal(false);
   }
 
+  const handleNotes = (title, description) => {
+      setData([...data, {title, description}]);
+    handleClose();
+  }
+
 
   
-
+console.log(data);
   return (
     <MainBox padding='3% 6%' border='2px solid black'>
     
     <AddNoteBtn handleClick={handleOpen}/>
-    <AddNoteDialog open={openModal} close={handleClose}/>
+    <AddNoteDialog open={openModal} close={handleClose} addNote={handleNotes}/>
+    <Grid container spacing={3}>
+      {data.map((item, index)=>{
+       return(
+         <Grid item key={index}>
+          <Card>
+            <CardHeader
+            title={item.title}
+            />
+              
+            <CardContent>
+              <Typography>{item.description}</Typography>
+            </CardContent>
+          </Card>
+          </Grid>
+       )
+      })}
+    </Grid>
     </MainBox>
   );
 };
